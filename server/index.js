bf75611404922cf436f45324837453666256ee5e
Vineeth1512 <114067110+Vineeth1512@users.noreply.github.com>
2024-01-07 12:43:36 +0530

    added front end link

--- server/index.js
+++ server/index.js
@@ -4,13 +4,14 @@ const cors = require('cors')
 const RegisterModel = require('./models/Register')
 
 const app = express()
-app.use(cors(
-    {
-        origin: ["https://student-registration-front-end.vercel.app"],
-        methods: ["POST", "GET"],
-        credentials: true
-    }
-));
+app.use(cors());
+// app.use(cors(
+//     {
+//         origin: ["https://student-registration-front-end.vercel.app/"],
+//         methods: ["POST", "GET"],
+//         credentials: true
+//     }
+// ));
 app.use(express.json())
 
 mongoose.connect('mongodb+srv://dbUser:dbUserPassword@atlascluster.w6sb48g.mongodb.net/test?retryWrites=true&w=majority');
